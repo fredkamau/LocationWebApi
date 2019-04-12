606c897e3f4c71ae1c84c75d50004e67f29e741c
fredthedeveloper <fredrickamau18@gmail.com>
2019-04-12 20:42:47 +0300

    added a connection string to web.config

--- src/LocationWebApi/Controllers/LocationController.cs
+++ src/LocationWebApi/Controllers/LocationController.cs
@@ -1,14 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Net;
-using System.Net.Http;
 using System.Web.Http;
+using LocationDataAccess;
 
 namespace LocationWebApi.Controllers
 {
     public class LocationController : ApiController
     {
+        public IEnumerable<safaricom_regions> Get()
+        {
+            using (LocationDBEntities entities = new LocationDBEntities())
+            {
+                return entities.safaricom_regions.ToList();
+            }
+        }
 
     }
 }
